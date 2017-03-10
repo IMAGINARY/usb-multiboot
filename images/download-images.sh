fc2eaef4acc5822994c636a254fbcdd6a1f489a5
Christian Stussak <stussak@mfo.de>
2017-03-10 21:16:12 +0100

    Template based on Ubuntu 16.10 iso boot files

---
+++ images/download-images.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+wget -nc -O 2015-12-10_IMAGINARY_Lubuntu-amd64-14.04.2.iso https://cloud.imaginary.org/index.php/s/0022195ced786c8c963a609b147f3f64/download
+wget -nc http://cdimage.ubuntu.com/lubuntu/releases/16.10/release/lubuntu-16.10-desktop-amd64.iso
+wget -nc http://cdimage.ubuntu.com/lubuntu/releases/16.04/release/lubuntu-16.04-desktop-amd64.iso
+wget -nc http://cdimage.ubuntu.com/lubuntu/releases/16.04/release/lubuntu-16.04-desktop-i386.iso
+wget -nc http://cdimage.ubuntu.com/lubuntu/releases/14.04/release/lubuntu-14.04-desktop-amd64.iso
+wget -nc http://cdimage.ubuntu.com/lubuntu/releases/14.04/release/lubuntu-14.04-desktop-i386.iso
+wget -nc -O systemrescuecd-x86-4.9.3.iso https://downloads.sourceforge.net/project/systemrescuecd/sysresccd-x86/4.9.3/systemrescuecd-x86-4.9.3.iso?r=http%3A%2F%2Fwww.system-rescue-cd.org%2FDownload&ts=1489179328&use_mirror=vorboss
+wget -nc http://www.memtest.org/download/5.01/memtest86+-5.01.bin
