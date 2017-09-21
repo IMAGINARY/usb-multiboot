c244d8fc2ef7181870e9a5924c1496a12d37a962
Christian Stussak <stussak@mfo.de>
2017-09-21 11:04:34 +0200

    Add support for Fedora 26

--- images/download-images.sh
+++ images/download-images.sh
@@ -7,3 +7,4 @@ wget -nc http://cdimage.ubuntu.com/lubuntu/releases/14.04/release/lubuntu-14.04-
 wget -nc http://cdimage.ubuntu.com/lubuntu/releases/14.04/release/lubuntu-14.04-desktop-i386.iso
 wget -nc -O systemrescuecd-x86-4.9.3.iso https://downloads.sourceforge.net/project/systemrescuecd/sysresccd-x86/4.9.3/systemrescuecd-x86-4.9.3.iso?r=http%3A%2F%2Fwww.system-rescue-cd.org%2FDownload&ts=1489179328&use_mirror=vorboss
 wget -nc http://www.memtest.org/download/5.01/memtest86+-5.01.bin
+wget -nc https://download.fedoraproject.org/pub/fedora/linux/releases/26/Workstation/x86_64/iso/Fedora-Workstation-Live-x86_64-26-1.5.iso
